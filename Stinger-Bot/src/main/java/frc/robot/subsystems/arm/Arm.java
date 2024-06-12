c48f1033d0dbd57a5ea44f172de0299377b49505
kevin <kevinjaramillo247@gamil.com>
2024-06-12 11:00:24 -0700

    restructed file

--- Stinger-Bot/src/main/java/frc/robot/subsystems/arm/Arm.java
+++ Stinger-Bot/src/main/java/frc/robot/subsystems/arm/Arm.java
@@ -2,7 +2,6 @@ package frc.robot.subsystems.arm;
 
 import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.networktables.GenericEntry;
-import edu.wpi.first.wpilibj.DutyCycleEncoder;
 import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 // Imports go here
@@ -11,11 +10,11 @@ import frc.robot.Constants.ArmConstants;
 
 import static frc.robot.Constants.ArmConstants.kCurrentLimit;
 import static frc.robot.Constants.ArmConstants.kErrorTolerance;
-import static frc.robot.Constants.DIOConstants.encoderDioPort;
 import static frc.robot.Constants.ArmConstants.*;
 
 import java.util.function.DoubleSupplier;
 
+import com.ctre.phoenix6.configs.SlotConfigs;
 import com.ctre.phoenix6.configs.TalonFXConfiguration;
 import com.ctre.phoenix6.controls.Follower;
 import com.ctre.phoenix6.controls.MotionMagicVoltage;
@@ -26,19 +25,8 @@ import com.ctre.phoenix6.signals.NeutralModeValue;
 public class Arm extends SubsystemBase {
   // Misc variables for specific subsystem go here
 
-  // Enum representing all of the states the subsystem can be in
-  public enum PivotStates {
-    DefaultState,
-    AmpState,
-    ShooterState,
-    MidState,
-    CommunityShot,
-    CustomState
-    
-  }
-
-  public static PivotStates ArmCurrentState;
-  public static PivotStates ArmRequestedState;
+  public static double ArmRequestedState;
+  public static double ArmCurrentState;
 
   GenericEntry armError;
   GenericEntry armPos;
@@ -54,10 +42,7 @@ public class Arm extends SubsystemBase {
   private double desiredPosition = 0;
 
   //Constructor
-  public Arm(DoubleSupplier angle) {
-
-    this.angle = angle;
-    // Misc setup goes here
+  public Arm() {
 
     armError = Shuffleboard.getTab("Arm").add("Arm Errpr[Rotations]", 0).getEntry();
     armPos = Shuffleboard.getTab("Arm").add("Arm Pos[Rotations]", 0).getEntry();
@@ -73,6 +58,7 @@ public class Arm extends SubsystemBase {
     var slot0Configs = talonFXConfigs.Slot0;
     slot0Configs.kS = kS;
     slot0Configs.kV = kV;
+    slot0Configs.kG = kG;
     slot0Configs.kP = kP;
     slot0Configs.kI = kI;
     slot0Configs.kD = kD;
@@ -102,8 +88,8 @@ public class Arm extends SubsystemBase {
     leadKraken.getConfigurator().apply(talonFXConfigs);
     followKraken.getConfigurator().apply(talonFXConfigs);
 
-    ArmCurrentState = PivotStates.DefaultState;
-    ArmRequestedState = PivotStates.DefaultState;
+    ArmCurrentState = 0;
+    ArmRequestedState = 0;
 
     // if we design the robot with a proper resting position in mind
     // this should be the only initilization necessary
@@ -114,42 +100,25 @@ public class Arm extends SubsystemBase {
 
   @Override
   public void periodic() {
-
     armError.setDouble(getError());
     armPos.setDouble(getPosition());
 
-    switch (ArmRequestedState) {
-      case DefaultState:
-        desiredPosition = 0;
-        break;
-      case AmpState:
-        desiredPosition = 0.23;//Origin: .22
-        break;
-      case ShooterState:
-        desiredPosition = MathUtil.clamp(angle.getAsDouble(), 0, .22);
-        break;
-      case CommunityShot:
-        desiredPosition = 0.024;
-        break;
-      case MidState:
-        desiredPosition = 0.044;// actual mid position should be around 0.13185
-        break;
+    setPosition();
+
+    SmartDashboard.putBoolean ( "Is At Setpoint", armAtSetpoint());
+    
     }
 
-    SmartDashboard.putBoolean("Climber at Setpoint", climberAtSetpoint());
- 
-    runControlLoop();
 
-    if (getError() < kErrorTolerance)
-      ArmCurrentState = ArmRequestedState;
-    else
-      ArmCurrentState = PivotStates.CustomState;  
-  }
-
-  public void runControlLoop() {
+  public void setPosition() {
     //leadKraken.setVoltage(.45);
     leadKraken.setControl(request.withPosition(desiredPosition));
     followKraken.setControl(new Follower(leadID , true));
+  }
+
+
+  public void requestPosition(double requestedPosition){
+   desiredPosition = requestedPosition; 
   }
 
   private double getPosition() {
@@ -160,17 +129,8 @@ public class Arm extends SubsystemBase {
     return Math.abs(getPosition() - desiredPosition);
   }
  
-  // example of a "setter" method
-  public void requestState(PivotStates requestedState) {
-    ArmRequestedState = requestedState;
-  }
- 
-  // example of a "getter" method
-  public PivotStates getCurrentState() {
-    return ArmCurrentState;
-  }
 
-  public boolean climberAtSetpoint(){
+  public boolean armAtSetpoint(){
     if(getError() <= ArmConstants.kErrorTolerance){
       return true;
     } else{
